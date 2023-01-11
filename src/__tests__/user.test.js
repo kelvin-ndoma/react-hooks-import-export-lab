c554198a244c1bacbf248a9a01613b201a0927df
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-11 16:39:35 +0300

    completed

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -4,4 +4,6 @@ test("username, city, and image are all exported", () => {
   expect(username).toEqual("Liza");
   expect(city).toEqual("New York");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
-});
+})
+
+//export{username, city, image}
